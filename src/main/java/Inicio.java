517c5667a9bee43a568838a061d978557f28b67f
Gabriel Villegas <nspooky@outlook.com>
2020-06-03 19:49:31 -0500

    Creacion de menu

--- src/main/java/Inicio.java
+++ src/main/java/Inicio.java
@@ -1,13 +1,40 @@
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Inicio {
     public static void main(String[] args) {
-        Conexion c = new Conexion();
-        try {
-            Connection cnt = c.get_connetion();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+
+        do {
+            System.out.println("---------------");
+            System.out.println("Aplicacion de mensajes");
+            System.out.println("1. Crear un mensaje");
+            System.out.println("2. Listar mensajes");
+            System.out.println("3. Editar mensaje");
+            System.out.println("4. Eliminar mensaje");
+            System.out.println("5. Salir");
+
+            //Leer opcion del usuario
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    MensajesService.crearMensaje();
+                    break;
+                case 2:
+                    MensajesService.listarMensajes();
+                    break;
+                case 3:
+                    MensajesService.editarMensaje();
+                    break;
+                case 4:
+                    MensajesService.borrarMensaje();
+                    break;
+                default:
+                    break;
+            }
+        }while (opcion != 5);
+
     }
 }
