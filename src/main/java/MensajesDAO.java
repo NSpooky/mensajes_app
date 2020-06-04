43c8d544abc471d03e548d93542accbe41cea3da
Gabriel Villegas <nspooky@outlook.com>
2020-06-03 20:14:00 -0500

    Creacion metodo guardar mensajes en BD

--- src/main/java/MensajesDAO.java
+++ src/main/java/MensajesDAO.java
@@ -1,6 +1,26 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MensajesDAO {
     public static void crearMensajeDB(Mensajes mensaje){
+        Conexion db_connect = new Conexion();
 
+        try (Connection conexion = db_connect.get_connetion()){
+            PreparedStatement ps = null;
+            try {
+                String query = "INSERT INTO `mensajes` (`mensaje`, `autor_mensaje`) VALUES (?, ?)";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutor_mensaje());
+                ps.executeUpdate();
+                System.out.println("El mensaje fue creado correctamente");
+            }catch (SQLException e) {
+                System.out.println(e);
+            }
+        }catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 
     public static void leerMensajeDB(){
